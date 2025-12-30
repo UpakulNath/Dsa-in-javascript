f0b802036cbf43cf849e323246137f96516c2c11
Upakul <upakuln1@gmail.com>
2025-12-30 09:54:52 +0530

    3 array questions

--- arrayProblems.js
+++ arrayProblems.js
@@ -71,6 +71,7 @@ arr[arr.length - 1] = temp
 console.log(arr);
 */
 
+/*
 let arr = [1, 2, 3, 4, 5, 6, 7]
 let k = 2
 for (let i = 0; i < k; i++){
@@ -81,5 +82,92 @@ for (let i = 0; i < k; i++){
   arr[0] = temp
 }
 console.log(arr);
+*/
+
+/*
+function reverseArray(arr, startIdx, endIdx) {
+  while (startIdx < endIdx) {
+    let temp = arr[startIdx]
+    arr[startIdx] = arr[endIdx]
+    arr[endIdx] = temp
+    startIdx++
+    endIdx--
+  }
+}
+
+function rotateArray(arr, k, direction) {
+  let arrLength = arr.length
+
+  if (arrLength == 0 || k == 0) return
+  
+  k = k % arrLength
+
+  if (direction.toLowerCase() == "right") {
+    reverseArray(arr, 0, arrLength - 1)
+    reverseArray(arr, 0, k - 1)
+    reverseArray(arr, k, arrLength - 1)
+  } else if (direction.toLowerCase() == "left") {
+    reverseArray(arr, 0, k - 1)
+    reverseArray(arr, k, arrLength - 1)
+    reverseArray(arr, 0, arrLength - 1)
+  }
+  return arr
+  
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 2, "left"));
+*/
+
+/*
+let arr = [ 1, 2, 3, 4, 5]
+
+let i, j
+let found = false
+
+for (let k = 0; k < arr.length; k++){
+  if (arr[k] === 0) {
+    i = k
+    j = k + 1
+    found = true
+    break
+  }
+}
+
+if (found) {
+  
+while (j < arr.length) {
+  if (arr[i] !== 0) {
+    i++
+  } 
+  if (arr[j] !== 0) {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    j++
+  } else {
+    j++
+  }
+
+}
+
+console.log(arr);
+} else {
+  console.log("no 0 element found");
+  
+}
+*/
+
+let arr = [1, 4, 5]
+let num = 23
+let found = false
+
+for (let i = 0; i < arr.length; i++){
+  if (arr[i] === num) {
+    console.log(i);
+    found = true
+    
+  }
+}
+if(found == false)console.log("-1");
 
 
